83fff21f4d0166f146773b6dab60378482b94b79
Michael Sagnes <michael@northernweb.no>
2021-08-26 15:11:01 +0200

    URL reference API
    Appended /api in start of route in order to make clearer route 
    distinctions.

--- routes/logitem.routes.js
+++ routes/logitem.routes.js
@@ -2,20 +2,17 @@ module.exports = app => {
   const logitemController = require("../controllers/logitem.controller.js");
 
   // Create a new Log Item
-  //app.post("/customers", logitemController.create);
+  //app.post("/api/logitems", logitemController.create);
 
   // Retrieve all Log Item
-  app.get("/logitems", logitemController.findAll);
+  app.get("/api/logitems", logitemController.findAll);
 
   // Retrieve a single Log Item with logitemId
-  app.get("/logitems/:logitemId", logitemController.findOne);
+  app.get("/api/logitems/:logitemId", logitemController.findOne);
 
   // Update a Log Item with logitemId
-  //app.put("/customers/:customerId", logitemController.update);
+  //app.put("/api/logitems/:logitemId", logitemController.update);
 
-  // Delete a Log Item with logItemId
-  //app.delete("/customers/:customerId", logitemController.delete);
-
-  // Create a new Log Item
-  //app.delete("/customers", logitemController.deleteAll);
+  // Delete a Log Item with logitemId
+  //app.delete("/api/logitems/:logitemId", logitemController.delete);
 };
