65cbd3112e2072d8d12cf9d3ddc4c0e46fc1f5bf
Northern-Web <michael@northernweb.no>
2021-09-07 00:30:50 +0200

    Authorization of routes.
    Added authorization layer of certain routes.

--- routes/logitem.routes.js
+++ routes/logitem.routes.js
@@ -1,3 +1,5 @@
+const authorize = require("../_middleware/authorize.js");
+
 module.exports = app => {
   const logitemController = require("../controllers/logitem.controller.js");
 
@@ -5,10 +7,10 @@ module.exports = app => {
   //app.post("/api/logitems", logitemController.create);
 
   // Retrieve all Log Item
-  app.get("/api/logitems", logitemController.findAll);
+  app.get("/api/logitems", authorize, logitemController.findAll);
 
   // Retrieve a single Log Item with logitemId
-  app.get("/api/logitems/:logitemId", logitemController.findOne);
+  app.get("/api/logitems/:logitemId", authorize, logitemController.findOne);
 
   // Update a Log Item with logitemId
   //app.put("/api/logitems/:logitemId", logitemController.update);
