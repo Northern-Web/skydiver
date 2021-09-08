dcd87fad8fe6b6e90a0a54be447c804b1cc12b51
Northern-Web <michael@northernweb.no>
2021-09-08 19:12:34 +0200

    Added connection pooling
    Minimizing connection disruptions.

--- models/db.js
+++ models/db.js
@@ -2,17 +2,18 @@ const mysql = require("mysql");
 const dbConfig = require("../config/config.js");
 
 // Create a connection to the database
-const connection = mysql.createConnection({
-  host:     global.gConfig.database_host,
-  user:     global.gConfig.database_user,
-  password: global.gConfig.database_password,
-  database: global.gConfig.database
+const connection = mysql.createPool({
+  connectionLimit: 10,
+  host:            global.gConfig.database_host,
+  user:            global.gConfig.database_user,
+  password:        global.gConfig.database_password,
+  database:        global.gConfig.database
 });
 
 // open the MySQL connection
-connection.connect(error => {
+/*connection.connect(error => {
   if (error) throw error;
   console.log("Successfully connected to the database.");
-});
+});*/
 
 module.exports = connection;
