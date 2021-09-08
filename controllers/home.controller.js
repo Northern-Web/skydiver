43ac0633a6692d51d4b2bed8c4e4ed30ce3cf9a3
Northern-Web <michael@northernweb.no>
2021-09-08 19:13:48 +0200

    Added model reference
    Added reference to the logitem model

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -1,6 +1,16 @@
+const Logbook = require("../models/logitem.model.js");
+
 exports.getIndexPage = (req, res) => {
   res.status(200).render('home/index', {
     pageTitle: 'Skydiver',
     path: '/'
   });
 };
+
+exports.getDashboardPage = async (req, res) => {
+  res.status(200).render("home/dashboard", {
+    pageTitle: "Skydiving Logbook",
+    path: "/dashboard",
+    username: req.userData.name
+  });
+}
