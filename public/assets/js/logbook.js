be52183b128c0c698dd0e85c7468bc8a1d4431c3
Northern-Web <michael@northernweb.no>
2021-09-08 19:15:19 +0200

    Started on frontend javascript
    Making API-calls.

---
+++ public/assets/js/logbook.js
@@ -0,0 +1,36 @@
+async function GetLogbookItems () {
+  try {
+    ClearLogbookTable();
+    fetch("/api/logitems")
+    .then(response => response.json())
+    .then(data => {
+
+      for (let item of data) {
+        const date = new Date(item.jumpdate);
+        $("#logbook-table").find("tbody").append(`
+          <tr>
+          <td hidden>${item.jumpid}</td>
+          <td>${item.jumpnum}</td>
+          <td>${date.getDate()}.${date.getMonth()}.${date.getFullYear()}</td>
+          <td>${item.aircraft}</td>
+          <td>${item.dropzone}</td>
+          <td>${item.canopy}</td>
+          <td>${item.altitude}</td>
+          <td>${item.jumptype}</td>
+          <td class="${(item.approved) ? "text-success": "text-danger" }">${(item.approved) ? "<i class=\"material-icons\">done</i>" : "<i class=\"material-icons\">error</i>"}</td>
+          </tr>
+          `);
+      }
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+function ClearLogbookTable() {
+  $("#logbook-table > tbody").empty();
+}
+
+$( document ).ready(function() {
+    GetLogbookItems();
+});
