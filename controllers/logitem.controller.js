c884b94541be0fba4c9d2d872c672c2be6560c49
Northern-Web <michael@northernweb.no>
2021-09-08 19:17:09 +0200

    Started on create function
    Started on create function for logitems.

--- controllers/logitem.controller.js
+++ controllers/logitem.controller.js
@@ -1,9 +1,41 @@
 const LogItem = require("../models/logitem.model.js");
+const crypto  = require("crypto");
 
+exports.create = async (req, res) => {
+  // Validate request
+  if (!req.body) {
+    res.status(400).send({
+      message: "Content can not be empty!"
+    });
+  }
+
+  const currentJumps = await LogItem.countCurrentJumps(req.body.owner);
+
+  res.send("Retur: " + currentJumps);
+
+  /*// Create a Customer
+  const logItem = new LogItem({
+    jumpid: crypto.randomBytes(16).toString("hex"),
+    jumpdate: new Date(),
+    active: req.body.active
+  });
+
+// Save Customer in the database
+Customer.create(customer, (err, data) => {
+  if (err)
+    res.status(500).send({
+      message:
+        err.message || "Some error occurred while creating the Customer."
+    });
+  else res.send(data);
+});*/
+}
 
 // Retrieve all Customers from the database.
 exports.findAll = (req, res) => {
-  LogItem.getAll((err, data) => {
+  const OWNER = req.userData.userid;
+  console.log(OWNER);
+  LogItem.getAll(OWNER, (err, data) => {
   if (err)
     res.status(500).send({
       message:
